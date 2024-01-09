bdae442e14e4275a03dd874fc5961ec4d3ace56b
MUVERMA29 <156055871+MUVERMA29@users.noreply.github.com>
2024-01-09 17:56:22 +0530

    Update conn.js

--- DB/conn.js
+++ DB/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 
-const DB = "mongodb+srv://MuskanV:MuskanV@cluster0.lqpxk72.mongodb.net/auth?retryWrites=true&w=majority";
+const DB = "mongodb+srv://MuskanV:<password>@cluster0.lqpxk72.mongodb.net/<DB_name>?retryWrites=true&w=majority";
 
 mongoose.connect(DB,{
     useUnifiedTopology:true,
